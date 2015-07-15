0bf56d0bda96713d027237db0742420718997bcf
Claudia Mane <c@luperlabs.com>
2015-07-15 01:31:19 -0700

    Sticky header for scrolling up. General mockup design for frontpage.

--- js/freebsd.js
+++ js/freebsd.js
@@ -1,2 +1,21 @@
 // FreeBSD website.
 
+// Sticky Header
+(function () {
+  'use strict';
+  var lastScrollTop = $(window).scrollTop()
+
+  $(window).scroll(function(event){
+    var n = $(this).scrollTop();
+    if (n > lastScrollTop){
+       // downscroll
+      $('#header').removeClass("sticky");
+    } else {
+      // upscroll
+      $('#header').addClass("sticky");
+    }
+    lastScrollTop = n;
+    });
+
+})();
+
